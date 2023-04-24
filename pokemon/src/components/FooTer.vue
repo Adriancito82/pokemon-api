4fd18adf204a3e2df6d9c2caa1791dda3365cf88
adrian <profilte82@gmail.com>
2023-04-24 12:55:20 +0100

    Ya muestro los pokemon, ahora falta pulir

---
+++ pokemon/src/components/FooTer.vue
@@ -0,0 +1,44 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col cols="12">
+                <v-footer class="d-flex flex-column">
+                    <div class="bg-teal d-flex w-100 align-center px-4">
+                    <strong>Contacta con nuestras redes sociales!</strong>
+
+                    <v-spacer></v-spacer>
+
+                    <v-btn
+                        v-for="icon in icons"
+                        :key="icon"
+                        class="mx-4"
+                        :icon="icon"
+                        variant="plain"
+                        size="small"
+                    ></v-btn>
+                    </div>
+
+                    <div class="px-4 py-2 bg-black text-center w-100">
+                    {{ new Date().getFullYear() }} — <strong>Adrián Santana Suárez</strong>
+                    </div>
+                </v-footer>
+            </v-col>
+        </v-row>
+    </v-container>
+
+</template>
+
+<script>
+
+export default {
+    name: 'FooTer',
+
+    data: () => ({
+
+  }),
+}
+</script>
+
+<style>
+
+</style>
--- pokemon/src/components/HelloWorld.vue
+++ pokemon/src/components/HelloWorld.vue
@@ -14,6 +14,18 @@
         <h1 class="display-2 font-weight-bold mb-3">
           Aplicación para consultar todos los pokemon 🐱‍🚀
         </h1>
+        <input id="buscar" type="text" placeholder="Buscar Pokemon" v-model="namePokemon" @input="filter(namePokemon)">
+        <v-btn>Buscar</v-btn>
+
+        <div class="section" v-if="processed">
+            <CardPokemon v-for="(obj, i) in pokemons" :key="obj.id" v-bind:number="'#' + (i + 1)"
+            v-bind:name="obj.name" v-bind:image="obj.sprites.front_default" />
+        </div>
+
+        <div class="section" v-else>
+            <CardPokemon v-bind:number="'#' + pokemon.id" v-bind:name="pokemon.name" v-bind:image="pokemon.sprites.front_default" />
+        </div>
+
       </v-col>
     </v-row>
   </v-container>
@@ -21,17 +33,122 @@
 </template>
 
 <script>
+import CardPokemon from '@/components/CardPokemon.vue'
 
 export default {
-  name: 'HelloWorld',
+    name: 'HelloWorld',
+    components: {
+        CardPokemon,
+  },
 
-  data: () => ({
+    data: () => ({
+        pokemons: [],
+        namePokemon: '',
+        pokemon: {},
+        processed: true
+    }),
+    async created() {
+        for (let i = 1; i < 152; i++) {
+            fetch('https://pokeapi.co/api/v2/pokemon/' + i)
+                .then(async response => {
+                    const data = await response.json()
+                this.pokemons.push(data)
+                }).catch(() => {
+                console.error('Hay un error!!')
+            })
+        }
+    },
+    methods: {
+        filter(name) {
+            let regexp = /[^a-zA-Z]/gim
+            this.namePokemon = name.replace(regexp, '')
 
-  }),
+            if (name.length >= 3) {
+                fetch('https://pokeapi.co/api/v2/pokemon/' + name.toLowerCase())
+                    .then(async response => {
+                        const data = await response.json()
+                        this.pokemon = data
+                    this.processed = false
+                    }).catch(err => {
+                    console.log(err)
+                })
+            }
+
+            if (name == '') {
+                document.location.reload(true)
+            }
+        }
+  }
 }
 </script>
+
 <style>
 body {
     background-color: rgb(240, 240, 2);
+}
+input {
+    margin-left: 2em;
+    background-color: white;
+    border: solid black 2px;
+}
+/*@bottom-gray: #ededed;
+@color-medium-gray: #a9a9a9;*/
+main {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  /*background: @bottom-gray;*/
+  padding: 0 15px;
+}
+#campo-pesquisar {
+  outline: none;
+  margin: 20px 0;
+  width: 100%;
+  border-top: 0;
+  border-left: 0;
+  border-right: 0;
+  border-bottom: 2px solid gray;
+  background: gray;
+  color: gray;
+}
+section{width: 100%;}
+.section{
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  column-gap: 10px;
+  row-gap: 10px;
+}
+@media only screen and (max-width: 400px) {
+  .section{
+    grid-template-columns: 1fr;
+  }
+}
+@media only screen and (min-width: 401px) {
+  .section{
+    grid-template-columns: repeat(2, 1fr);
+  }
+}
+@media only screen and (min-width: 630px) {
+  .section{
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+@media only screen and (min-width: 900px) {
+  .section{
+    grid-template-columns: repeat(4, 1fr);
+  }
+}
+@media only screen and (min-width: 1257px) {
+  .section{
+    grid-template-columns: repeat(5, 1fr);
+  }
+}
+@media only screen and (min-width: 1500px) {
+  .section{
+    grid-template-columns: repeat(6, 1fr);
+  }
 }
 </style>
