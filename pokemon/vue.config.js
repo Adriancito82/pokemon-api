d4b02c5b4aa263dd01877a0f5303fb84801289a9
adrian <profilte82@gmail.com>
2023-05-03 11:05:58 +0100

    Done

--- pokemon/vue.config.js
+++ pokemon/vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
     transpileDependencies: true,
-    publicPath: '/pokemon',
+    publicPath: '/pokemon/',
 
   pluginOptions: {
     vuetify: {
